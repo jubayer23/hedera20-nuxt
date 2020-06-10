41950176f2ac7a78e73ee4ca4033a11f608eb7a8
Harshak777 <Harshak777@users.noreply.github.com>
2020-06-11 02:41:58 +0530

    added the nav bar pages

---
+++ pages/contact.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="how">
+    <h1>Contact:</h1>
+    <h2>Makarand Karanjalkar</h2>
+    <h5>Startup CEO/CTO DApp Dev</h5>
+    <a href="mak@spryetch.uk">Mail</a>.
+    <br />
+    <h2>Harshak Krishnaa K</h2>
+    <h5>Web Dev DApp Dev</h5>
+    <a href="harshakkrishnaa777@gmail.com">Mail</a>.
+    <br />
+  </div>
+</template>
