daa5f4bbf9af1ea4ed595d274a23371d0dfae4fa
Harshak777 <Harshak777@users.noreply.github.com>
2020-06-10 16:03:29 +0530

    added pictures

--- components/QuestionBox.vue
+++ components/QuestionBox.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="question-box-container">
     <b-jumbotron>
-      <template slot="lead">
-        {{ currentQuestion.question }}
-      </template>
+      <template slot="lead">{{ currentQuestion.question }}</template>
 
       <hr class="my-4" />
 
@@ -13,27 +11,21 @@
           :key="index"
           @click.prevent="selectAnswer(index)"
           :class="answerClass(index)"
-        >
-          {{ answer }}
-        </b-list-group-item>
+        >{{ answer }}</b-list-group-item>
       </b-list-group>
 
-      <b-button
+      <!--<b-button
         variant="primary"
         @click="submitAnswer"
         :disabled="selectedIndex === null || answered"
-      >
-        Submit
-      </b-button>
-      <b-button @click="next" variant="success">
-        Next
-      </b-button>
+      >Submit</b-button>-->
+      <b-button :disabled="selectedIndex === null" @click="next" variant="success">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
 
 <script>
-import _ from 'lodash'
+import _ from "lodash";
 
 export default {
   props: {
@@ -47,66 +39,81 @@ export default {
       correctIndex: null,
       shuffledAnswers: [],
       answered: false
-    }
+    };
   },
   computed: {
     answers() {
       // this function is no longer used in finished code
       // it is replaced by the watch function below and the
       // shuffleAnswers method
-      let answers = [...this.currentQuestion.incorrect_answers]
-      answers.push(this.currentQuestion.correct_answer)
-      return answers
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
     }
   },
   watch: {
     currentQuestion: {
       immediate: true,
       handler() {
-        this.selectedIndex = null
-        this.answered = false
-        this.shuffleAnswers()
+        this.selectedIndex = null;
+        this.answered = false;
+        this.shuffleAnswers();
       }
     }
   },
   methods: {
     selectAnswer(index) {
-      this.selectedIndex = index
-    },
-    submitAnswer() {
-      let isCorrect = false
+      this.selectedIndex = index;
+
+      let isCorrect = false;
 
       if (this.selectedIndex === this.correctIndex) {
-        isCorrect = true
+        isCorrect = true;
       }
-      this.answered = true
+      this.answered = true;
 
-      this.increment(isCorrect)
+      this.increment(isCorrect);
+    },
+    submitAnswer() {
+      let isCorrect = false;
+
+      if (this.selectedIndex === this.correctIndex) {
+        isCorrect = true;
+      }
+      this.answered = true;
+
+      this.increment(isCorrect);
     },
     shuffleAnswers() {
-      let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
-      console.log(answers)
-      this.shuffledAnswers = _.shuffle(answers)
-      this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+      let answers = [
+        ...this.currentQuestion.incorrect_answers,
+        this.currentQuestion.correct_answer
+      ];
+      console.log(answers);
+      this.shuffledAnswers = _.shuffle(answers);
+      this.correctIndex = this.shuffledAnswers.indexOf(
+        this.currentQuestion.correct_answer
+      );
     },
     answerClass(index) {
-      let answerClass = ''
+      let answerClass = "";
 
       if (!this.answered && this.selectedIndex === index) {
-        answerClass = 'selected'
+        answerClass = "selected";
       } else if (this.answered && this.correctIndex === index) {
-        answerClass = 'correct'
-      } else if (this.answered &&
+        answerClass = "correct";
+      } else if (
+        this.answered &&
         this.selectedIndex === index &&
         this.correctIndex !== index
       ) {
-        answerClass = 'incorrect'
+        answerClass = "incorrect";
       }
 
-      return answerClass
+      return answerClass;
     }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -115,7 +122,7 @@ export default {
 }
 
 .list-group-item:hover {
-  background: #EEE;
+  background: #eee;
   cursor: pointer;
 }
 
--- components/Result.vue
+++ components/Result.vue
@@ -1,51 +1,65 @@
 <template>
-    <div class="result">
-        <h1>You Scored {{numCorrect}} out of {{numTotal}}</h1>
-        <b-button size="lg" variant="primary" to="/">Play Again</b-button>
+  <div class="result">
+    <h2 v-if="numCorrect === numTotal">Congratulations Winner</h2>
+    <h2 v-else>
+      Failure is the stepping stones for success.
+      Never lose faith!! You can always try again to win!!
+    </h2>
+    <div class="bor">
+      <h1>You Scored {{numCorrect}} out of {{numTotal}}</h1>
+      <b-button size="lg" variant="primary" to="/">Play Again</b-button>
     </div>
+    <div class="vector">
+      <img src="./5482.jpg" :style="{'width': '500px'}" />
+    </div>
+  </div>
 </template>
 
 <script>
-
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    props: [
-        'numCorrect',
-        'numTotal'
-    ],
-    mounted: function() {
-        if(this.numCorrect === this.numTotal) {
-            alert("You Won")
-            const message = {
-                account: localStorage.account
-            }
-            console.log(message)
-            if (this.$route.params.amt === "five") {
-                axios.post('https://floating-basin-51607.herokuapp.com/hedera/fiveh', message)
-                .then(res => {
-
-                })
-                .catch(
-                    err => {
-                    console.error(err)
-                    }
-                );
-            } else {
-            
-                axios.post('https://floating-basin-51607.herokuapp.com/hedera/tenh', message)
-                .then(res => {
-
-                })
-                .catch(
-                    err => {
-                    console.error(err)
-                    }
-                );
-            
-            }
-            
-        }
+  props: ["numCorrect", "numTotal"],
+  mounted: function() {
+    if (this.numCorrect === this.numTotal) {
+      alert("You Won");
+      const message = {
+        account: localStorage.account
+      };
+      console.log(message);
+      if (this.$route.params.amt === "five") {
+        axios
+          .post(
+            "https://floating-basin-51607.herokuapp.com/hedera/fiveh",
+            message
+          )
+          .then(res => {})
+          .catch(err => {
+            console.error(err);
+          });
+      } else {
+        axios
+          .post(
+            "https://floating-basin-51607.herokuapp.com/hedera/tenh",
+            message
+          )
+          .then(res => {})
+          .catch(err => {
+            console.error(err);
+          });
+      }
+    }
   }
-}
+};
 </script>
+<style>
+.vector {
+  position: relative;
+  bottom: 0;
+}
+.bor {
+  border-style: inset;
+  padding: 20px;
+  border-radius: 15px;
+}
+</style>
